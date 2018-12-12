aaa311cc4a7e8d040fe8672e558e2fc57a1a6497
AndronovaYuliya <andron39933993@gmail.com>
2018-12-12 13:56:26 +0200

    edit includes in stylesheet.php

--- resources/views/includes/stylesheet.php
+++ resources/views/includes/stylesheet.php
@@ -4,7 +4,9 @@
 <link rel="stylesheet" href="../../css/bootstrap.css">
 
 <!-- Custom CSS -->
-<link rel="stylesheet" href="../../css/style.css">
+<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="/css/style.css">
+<link rel="stylesheet" href="<?php echo dirname($_SERVER['DOCUMENT_ROOT'],2).'/css/style.css'?>">
 
 <!-- Font Awesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
