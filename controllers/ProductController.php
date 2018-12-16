ce1ed9709b72d64bf2c8e2ea1f5773e5c82f43bb
AndronovaYuliya <andron39933993@gmail.com>
2018-12-16 23:52:10 +0200

    Made functions(for actions) all, show

---
+++ controllers/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+class ProductController {
+
+    private static $data=[];
+
+    /*public function __construct()
+    {
+        $this->data=$this->getProducts();
+    }
+*/
+    //show all products
+    public static function all()
+    {
+        return ProductModel::getProducts();
+    }
+
+    //show 1 product
+    public static function show($params,$query)
+    {
+        return ProductModel::getProduct($query);
+    }
+
+
+
+}
--- models/DataBaseModel.php
+++ models/DataBaseModel.php
@@ -1,9 +1,140 @@
 <?php
 class DataBaseModel{
-    public static function getDB(){
-        //+проверка
-        $data = file_get_contents('array.txt');
-        $data = unserialize($data);
-        return $data;
+    private static $data=[];
+    private static $array=[
+        [
+            'id'=>1,
+            'title'=>'Apple new mac book 2015 March',
+            'new_price'=>'899.00',
+            'old_price'=>'999.00',
+            'count'=>'10',
+            'visible'=>true,
+            'currency'=>'$',
+            'img'=>'/img/product-1.jpg'
+        ],[
+            'id'=>2,
+            'title'=>'Apple new mac book 2015 March',
+            'new_price'=>'899.00',
+            'old_price'=>'999.00',
+            'count'=>'10',
+            'visible'=>true,
+            'currency'=>'$',
+            'img'=>'/img/product-1.jpg'
+        ],[
+            'id'=>3,
+            'title'=>'Apple new mac book 2015 March',
+            'new_price'=>'899.00',
+            'old_price'=>'999.00',
+            'count'=>'10',
+            'visible'=>true,
+            'currency'=>'$',
+            'img'=>'/img/product-1.jpg'
+        ],[
+            'id'=>4,
+            'title'=>'Apple new mac book 2015 March',
+            'new_price'=>'899.00',
+            'old_price'=>'999.00',
+            'count'=>'10',
+            'visible'=>true,
+            'currency'=>'$',
+            'img'=>'/img/product-1.jpg'
+        ],[
+            'id'=>5,
+            'title'=>'Apple new mac book 2015 March',
+            'new_price'=>'899.00',
+            'old_price'=>'999.00',
+            'count'=>'10',
+            'visible'=>true,
+            'currency'=>'$',
+            'img'=>'/img/product-1.jpg'
+        ],[
+            'id'=>6,
+            'title'=>'Apple new mac book 2015 March',
+            'new_price'=>'899.00',
+            'old_price'=>'999.00',
+            'count'=>'10',
+            'visible'=>true,
+            'currency'=>'$',
+            'img'=>'/img/product-1.jpg'
+        ],[
+            'id'=>7,
+            'title'=>'Apple new mac book 2015 March',
+            'new_price'=>'899.00',
+            'old_price'=>'999.00',
+            'count'=>'10',
+            'visible'=>true,
+            'currency'=>'$',
+            'img'=>'/img/product-1.jpg'
+        ],[
+            'id'=>8,
+            'title'=>'Apple new mac book 2015 March',
+            'new_price'=>'899.00',
+            'old_price'=>'999.00',
+            'count'=>'10',
+            'visible'=>true,
+            'currency'=>'$',
+            'img'=>'/img/product-1.jpg'
+        ],[
+            'id'=>9,
+            'title'=>'Apple new mac book 2015 March',
+            'new_price'=>'899.00',
+            'old_price'=>'999.00',
+            'count'=>'10',
+            'visible'=>true,
+            'currency'=>'$',
+            'img'=>'/img/product-1.jpg'
+        ],[
+            'id'=>10,
+            'title'=>'Apple new mac book 2015 March',
+            'new_price'=>'899.00',
+            'old_price'=>'999.00',
+            'count'=>'10',
+            'visible'=>true,
+            'currency'=>'$',
+            'img'=>'/img/product-1.jpg'
+        ],[
+            'id'=>11,
+            'title'=>'Apple new mac book 2015 March',
+            'new_price'=>'899.00',
+            'old_price'=>'999.00',
+            'count'=>'10',
+            'visible'=>true,
+            'currency'=>'$',
+            'img'=>'/img/product-1.jpg'
+        ],[
+            'id'=>12,
+            'title'=>'Apple new mac book 2015 March',
+            'new_price'=>'899.00',
+            'old_price'=>'999.00',
+            'count'=>'10',
+            'visible'=>true,
+            'currency'=>'$',
+            'img'=>'/img/product-1.jpg'
+        ]
+    ];
+
+    public function __construct()
+    {
+
+    }
+
+    public static function getData($fileName):array
+    {
+        self::setDB(self::$array,$fileName);
+        /*self::$data=[];*/
+        self::$data = file_get_contents($fileName);
+        self::$data = unserialize(self::$data);
+        return self::$data;
+    }
+
+
+    public static function setDB($array, $fileName)
+    {
+        self::$data = serialize($array);
+        echo 4;
+        echo $fileName;
+        echo 4;
+        die ();
+        file_put_contents($fileName, self::$data);
     }
 }
