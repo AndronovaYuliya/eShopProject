946210b589ac92854a2b7d52104c1dcdd168a5b3
AndronovaYuliya <andron39933993@gmail.com>
2018-12-16 23:53:53 +0200

    made functions parseUrl, routing

--- routers/Router.php
+++ routers/Router.php
@@ -1,24 +1,33 @@
 <?php
 class Router
 {
+    private $fileExtension='.php';
+    private $_needle='Controller';
+    private $nameController;
+    private $nameAction;
+    private $requestMethod;
+    private $params=[];
+    private $data=[];
+    private $query=[];
+
     public function __construct()
     {
-        echo "Router";
-        die();
+
     }
 
-    private static $url;
-    public static function setUrl($url='/')
+
+   /* public static function setUrl($url='/')
     {
         self::$url=ParseURLModel::parseUrl($url);
 
-    }
-    function ErrorPage404()
+    }*/
+    static function ErrorPage404()
     {
-        /*$host = 'http://'.$_SERVER['HTTP_HOST'].'/';
+        $host = 'http://'.$_SERVER['HTTP_HOST'].'/';
         header('HTTP/1.1 404 Not Found');
         header("Status: 404 Not Found");
-        header('Location:'.$host.'404');*/
+        header('Location:'.$host);
+        //header('Location:'.$host.'404');
     }
 
     /*
@@ -29,27 +38,57 @@ class Router
         $url = route('profile', ['id' => 1]);*/
 
 
-    static public function routing(/*string, $callback (function or UserController@index)*/){
 
-
-        /*echo "<pre>";
-        var_dump($_SERVER)
-;        echo "</pre>";*/
+    public function routing()
+    {
+        $this->parseUrl();
+        //+обработать ошибку
+        if($this->nameController!=='Controller')
+        {
+            $controller =$this->nameController;
+            $action = $this->nameAction;
+            if (method_exists($controller, $action))
+            {
+                $this->data = $controller::$action($this->params, $this->query);
+                echo "<pre>";
+                var_dump($this->data);
+                echo "</pre>";
+            }
+            else
+            {
+                // кинуть исключение
+                self::ErrorPage404();
+            }
+        }
 
     }
-    static function get(/*string, $callback (function or UserController@index)*/){}
-    static function post($uri, $callback){}
-    static function put($uri, $callback){}
-    static function patch($uri, $callback){}
-    static function delete($uri, $callback){}
-    static function options($uri, $callback){}
-    static function match(/*['get', 'post'] , '/', function () {}*/){}
-    static function any(/*'foo', function () {}*/){}
-    static function group(){}
-    //Redirect Routes
-    static function redirect(/*'/here', '/there',301*/){}
-    static function permanentRedirect(/*'/here', '/there',301*/){}
-    //view routers
+
+    private function parseUrl()
+    {
+        $arrayUrl=parse_url($_SERVER['REQUEST_URI']);
+
+        //Parse path
+        $path=explode('/',$arrayUrl['path']);
+        array_shift($path);
+        $this->nameController=ucfirst(array_shift($path)).$this->_needle;
+        $this->nameAction= array_shift($path);
+        $this->params= $path;
+        $this->requestMethod=$_SERVER['REQUEST_METHOD'];
+
+        //Parse query
+        if(array_key_exists('query',$arrayUrl))
+        {
+            $query=explode(',',$arrayUrl['query']);
+            $this->query=[];
+            foreach ($query as $key=>$value)
+            {
+                $tmp=explode('=',$value);
+                $this->query[$tmp[0]]=$tmp[1];
+            }
+        }
+    }
+
+
     static function view(/*'/welcome', 'welcome', ['name' => 'Taylor']*/){}
     //Route Prefixes
     /*Route::prefix('admin')->group(function () {
