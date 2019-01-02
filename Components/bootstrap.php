aab397173d7e537afd819e533afce764e2b90e17
AndronovaYuliya <andron39933993@gmail.com>
2019-01-02 13:55:49 +0200

    add andronova-yuliya/monolog-packagist

--- Components/bootstrap.php
+++ Components/bootstrap.php
@@ -2,6 +2,9 @@
 
 require dirname(__FILE__,2). '/vendor/autoload.php';
 
-
+use CostumLogger\CostumLogger;
 use Components\Core\Router;
+
+$log=new CostumLogger();
 Router::routing();
+$log->warning('Costum warning');
